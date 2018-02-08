f4cfba576c151b35293403bd379b8ea268dcf9f9
Heiko Finzel <hfi@boreus.de>
2018-02-08 04:04:04 +0000

    autoscaling

--- ec2helper/instance.py
+++ ec2helper/instance.py
@@ -18,32 +18,34 @@ class Instance(object):
         self.id = instance_id
         self.region = region
 
+##### tags ####################################################################
+
     @property
     def tags(self):
-        """                                                                     
+        """
         Get this instance's tags from API (as dict).
-        """                                    
+        """
         client = boto3.client("ec2", region_name=self.region)
         response = client.describe_tags(
             Filters=[{
                 "Name": "resource-id",
                 "Values": [self.id]
             }]
-        )                                 
+        )
         assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
         return tags_to_dict(response["Tags"])
 
-    @tags.setter                                                         
-    def tags(self, value):                                               
+    @tags.setter
+    def tags(self, value):
         """
         Same as update_tags.
         """
         self.update_tags(**value)
 
     def update_tags(self, **kwargs):
-        """                                                                     
+        """
         Update this instance's tags via API (from dict).
-        """                                                                     
+        """
         client = boto3.client("ec2", region_name=self.region)
         response = client.create_tags(
             Resources=[self.id],
@@ -61,3 +63,117 @@ class Instance(object):
             Tags=[{"Key": k} for k in args]
         )
         assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+##### autoscaling #############################################################
+
+    @property
+    def autoscaling(self):
+        """
+        Get autoscaling status for this instance, None if it is no autoscaling
+        instance.
+
+        {
+            "AutoScalingGroupName": "hfi-amazon2-boreus-ami",
+            "AvailabilityZone": "eu-central-1b",
+            "HealthStatus": "HEALTHY",
+            "InstanceId": "i-0d2cb773a18dfa487",
+            "LaunchConfigurationName": "hfi-amazon2-boreus-ami",
+            "LifecycleState": "InService",
+            "ProtectedFromScaleIn": false
+        }
+        """
+        client = boto3.client("autoscaling", region_name=self.region)
+        response = client.describe_auto_scaling_instances(
+            InstanceIds=[self.id]
+        )
+        assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+        return response["AutoScalingInstances"][0] if response[
+            "AutoScalingInstances"] else None
+
+    @property
+    def autoscaling_protected(self):
+        """
+        Test if this instance is protected against scale in events.
+        """
+        data = self.autoscaling
+        return data["ProtectedFromScaleIn"] if data is not None else True
+
+    @autoscaling_protected.setter
+    def autoscaling_protected(self, value):
+        """
+        Set protection against scale in events.
+        """
+        data = self.autoscaling
+        if data is None: return
+        client = boto3.client("autoscaling", region_name=self.region)
+        response = client.set_instance_protection(
+            InstanceIds=[self.id],
+            AutoScalingGroupName=data["AutoScalingGroupName"],
+            ProtectedFromScaleIn=bool(value)
+        )
+        assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+    @property
+    def autoscaling_healthy(self):
+        """
+        Test if this instance is protected against scale in events.
+        """
+        data = self.autoscaling
+        return data["HealthStatus"] == "HEALTHY"
+
+    @autoscaling_health.setter
+    def autoscaling_healthy(self, value):
+        """
+        Set protection against scale in events.
+        """
+        data = self.autoscaling
+        if data is None: return
+        client = boto3.client("autoscaling", region_name=self.region)
+        response = client.set_instance_health(
+            InstanceId=self.id,
+            ShouldRespectGracePeriod=False,
+            HealthStatus="Healthy" if value else "Unhealthy"
+        )
+        assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+    @property
+    def autoscaling_standby(self):
+        """
+        Test if this instance is in standby mode.
+        """
+        data = self.autoscaling
+        return data["LifecycleState"] == "Standby"
+
+    @autoscaling_standby.setter
+    def autoscaling_standby(self, value):
+        """
+        Seamless standby mode.
+        TODO: doesn't work yet, we have to manually adjust capacity.
+        botocore.exceptions.ClientError: An error occurred (ValidationError)
+            when calling the EnterStandby operation: AutoScalingGroup
+            hfi-amazon2-boreus-ami has min-size=2, max-size=2, and
+            desired-size=2. To place into standby 1 instances, please update
+            the AutoScalingGroup sizes appropriately.
+        ShouldDecrementDesiredCapacity=False would help, but not with
+        exit_standby
+        """
+        data = self.autoscaling
+        if data is None: return
+        client = boto3.client("autoscaling", region_name=self.region)
+        #data["LifecycleState"] == "InService"
+        if value:
+            #response = client.put_scaling_policy(
+            #    AutoScalingGroupName=data["AutoScalingGroupName"],
+            #)
+            response = client.enter_standby(
+                InstanceIds=[self.id],
+                AutoScalingGroupName=data["AutoScalingGroupName"],
+                ShouldDecrementDesiredCapacity=True
+            )
+            assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+        elif data["LifecycleState"] == "Standby":
+            response = client.exit_standby(
+                InstanceIds=[self.id],
+                AutoScalingGroupName=data["AutoScalingGroupName"]
+            )
+            assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
--- foo.py
+++ foo.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals, absolute_import
 from ec2helper import Instance, json_dump, get_instance_tags_by_tag
 import ec2helper.utils
 import boto3
+import time
 
 i = Instance()
 #tags = i.tags
@@ -12,10 +13,6 @@ i = Instance()
 
 #def lock(locktag, searchtag=locktag, searchvalue=None)
 
-client = boto3.client('autoscaling', region_name=i.region)
-response = client.describe_auto_scaling_instances(
-    InstanceIds=[i.id]
-)
 """
 {
     "AutoScalingInstances": [], 
@@ -31,8 +28,40 @@ response = client.describe_auto_scaling_instances(
         "RetryAttempts": 0
     }
 }
+{
+    "AutoScalingInstances": [
+        {
+"AutoScalingGroupName": "hfi-amazon2-boreus-ami", 
+            "AvailabilityZone": "eu-central-1b", 
+   "HealthStatus": "HEALTHY", 
+            "InstanceId": "i-0d2cb773a18dfa487", 
+            "LaunchConfigurationName": "hfi-amazon2-boreus-ami", 
+   "LifecycleState": "InService", 
+"ProtectedFromScaleIn": false
+        }
+    ], 
+    "ResponseMetadata": {
+        "HTTPHeaders": {
+            "content-length": "833", 
+            "content-type": "text/xml", 
+            "date": "Wed, 07 Feb 2018 16:12:41 GMT", 
+            "x-amzn-requestid": "b959e409-0c21-11e8-9ea5-f90d60e343bb"
+        }, 
+        "HTTPStatusCode": 200, 
+        "RequestId": "b959e409-0c21-11e8-9ea5-f90d60e343bb", 
+        "RetryAttempts": 0
+    }
+}
 """
 
 
 #json_dump(get_instance_tags_by_tag("MetricGroup"))#, "CachingProxyTest"))
-json_dump(response)
+json_dump(i.autoscaling)
+i.autoscaling_healthy = False
+json_dump(i.autoscaling)
+time.sleep(10)
+json_dump(i.autoscaling)
+i.autoscaling_healthy = True
+json_dump(i.autoscaling)
+time.sleep(10)
+json_dump(i.autoscaling)
